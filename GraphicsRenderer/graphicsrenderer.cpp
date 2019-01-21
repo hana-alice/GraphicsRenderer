51ac9bd0fe7a6f2412a577606dd1b9b80b460164
Zeqiang LI <mingmingmou@gmail.com>
2019-01-21 22:59:55 +0800

    keep lighting

--- GraphicsRenderer/graphicsrenderer.cpp
+++ GraphicsRenderer/graphicsrenderer.cpp
@@ -69,6 +69,7 @@ glm::vec3 camRight = glm::normalize(glm::cross(up, camDirection));
 //glm::vec3 camUp = glm::normalize(glm::cross(camDirection, camRight));
 glm::vec3 camUp = glm::vec3(0.0f, 1.0f, 0.0f);
 glm::vec3 camStraitFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
 
 int main()
 {
@@ -104,18 +105,20 @@ int main()
 	glm::vec3 result = lightColor * coral;
 
 
-	Shader ourShader("vertexShader.vs", "fragShader.fs");
+	Shader objShader("vertexShader.vs", "fragShader.fs");
 
-	unsigned int objVBO, objVAO;
+	unsigned int objVBO, objVAO, objEBO;
 	glGenVertexArrays(1, &objVAO);
 	glBindVertexArray(objVAO);
 	glGenBuffers(1, &objVBO);
+	glGenBuffers(1, &objEBO);
+	glBindVertexArray(objVAO);
 	glBindBuffer(GL_ARRAY_BUFFER, objVBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 	GLint objPosLoc, objColorLoc;
-	objPosLoc = glGetAttribLocation(ourShader.ID, "aPos");
-	objColorLoc = glGetUniformLocation(ourShader.ID, "inColor");
+	objPosLoc = glGetAttribLocation(objShader.ID, "aPos");
+	objColorLoc = glGetUniformLocation(objShader.ID, "inColor");
 	glEnableVertexAttribArray(objPosLoc);
 
 	glm::mat4 modelMat(1.0f), projMat(1.0f), viewMat(1.0f);
@@ -123,10 +126,18 @@ int main()
 	projMat = glm::perspective(glm::radians(45.0f), float(SCR_WIDTH / SCR_HEIGHT), 0.1f, 100.0f);
 	viewMat = glm::lookAt(camPos, camPos + camStraitFront, camUp);
 
-	
+	objShader.use();
 
-	ourShader.use();
-
+	unsigned int lightVAO;
+	glGenVertexArrays(1, &lightVAO);
+	glBindVertexArray(lightVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, objVBO);//same with object
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), 0);
+	glEnableVertexAttribArray(0);
+	Shader lightShader("vertexShader.vs", "fragShader.fs");
+	lightShader.use();
+	lightShader.setVec3("lightColor", 1.0f, 0.5f, 0.31f);
+	lightShader.setVec3("objColor", 1.0f, 1.0f, 1.0f);
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -137,15 +148,15 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		
-		ourShader.use();
+
+		objShader.use();
 		glBindVertexArray(objVAO);
 		glBindBuffer(GL_ARRAY_BUFFER, objVBO);
 
 		GLint modelLoc, viewLoc, projLoc;
-		modelLoc = glGetUniformLocation(ourShader.ID, "model");
-		viewLoc = glGetUniformLocation(ourShader.ID, "view");
-		projLoc = glGetUniformLocation(ourShader.ID, "projection");
+		modelLoc = glGetUniformLocation(objShader.ID, "model");
+		viewLoc = glGetUniformLocation(objShader.ID, "view");
+		projLoc = glGetUniformLocation(objShader.ID, "projection");
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(modelMat));
 		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(viewMat));
 		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projMat));
@@ -154,8 +165,8 @@ int main()
 		glUniform4f(objColorLoc, 1.0f, 1.0f, 0.0f, 1.0f);
 
 		glDrawArrays(GL_TRIANGLES, 0, 36);
-		
-		
+
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
