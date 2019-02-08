57fb41a3e898f640484d7c50656ce28375db952e
Zeqiang <mingmingmou@gmail.com>
2019-02-08 19:57:44 +0800

    mesh

--- GraphicsRenderer/mesh.cpp
+++ GraphicsRenderer/mesh.cpp
@@ -1,1 +1,12 @@
 #include "mesh.h"
+
+Mesh::Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<Texture> textures)
+    :m_vertices(vertices),m_indices(indices),m_textures(textures)
+{
+    setupMesh();
+}
+
+void Mesh::setupMesh()
+{
+    
+}
