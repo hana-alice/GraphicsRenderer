ecd4e985bb022ab04d3772ca7001b1f1530550ae
LevisYoung <mingmingmou@gmail.com>
2019-07-05 16:30:39 +0800

    struct in shader

--- resources/shader/object.fs
+++ resources/shader/object.fs
@@ -1,4 +1,23 @@
 #version 330 core
+
+struct Material
+{
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;
+};
+
+struct Light
+{
+    vec3 lightPos;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+uniform Light light;
+uniform Material material;
+
 out vec4 FragColor;
 in vec3 Normal;
 in vec3 FragPos;
@@ -9,24 +28,23 @@ uniform vec3 viewPos;
 
 void main()
 {
-
     //--------phong's model
     //ambient
     float ambientStrength = 0.1;
-    vec3 ambient = ambientStrength * lightColor;
+    vec3 ambient = light.ambient * material.ambient;
 
     //diffuse
     vec3 norm = normalize(Normal);
     vec3 lightDir =normalize(lightPos - FragPos);
     float diff = max(dot(lightDir,norm),0.0);
-    vec3 diffuse = diff * lightColor;
+    vec3 diffuse = diff * material.diffuse * light.diffuse;
 
     //specular
     float specularStrength = 0.5;
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir,norm);
-    float spec = pow(max(dot(viewDir,reflectDir),0.0),32);
-    vec3 specular = specularStrength * spec * lightColor;
+    float spec = pow(max(dot(viewDir,reflectDir),0.0),material.shininess);
+    vec3 specular = material.specular * spec * light.specular;
 
     vec3 result = (specular + ambient + diffuse) * objectColor;
     FragColor = vec4(result, 1.0);
